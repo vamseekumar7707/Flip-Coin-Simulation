dd84c7f69e14c45ed2935129e2368622c46f89ed
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-13 18:21:07 +0530

    Added: Display Head / Tail

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,2 +1,30 @@
 #!/bin/bash -x
-#WELOCME MESSAGE
+declare -A singlet
+
+#FOR SINGLET
+function flipCoin() {
+        n=$1
+        for (( i=0; i<$n; i++ ))
+        do
+        for (( j=0; j<20; j++ ))
+        do
+                random1=$((RANDOM%2))
+
+                if [ $n -eq 1 ]
+                then
+                        if [ $random1 -eq 0 ]
+                        then
+                singlet["$j"]="Head |"
+                        ((hcount++))
+                        else
+                singlet["$j"]="Tail |"
+                        ((tcount++))
+                        fi
+                fi
+	done
+	done
+}
+flipCoin 1
+
+echo "SINGLET:::" ${singlet[@]}
+
